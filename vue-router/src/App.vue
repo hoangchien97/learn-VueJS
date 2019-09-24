ff2189ca02ab8d37e4b2bf2933d821284603f1e6
Chien Hoang <hoangducchien97@gmail.com>
2019-09-24 17:24:44 +0700

    using this.$router .push for btn click as 1 router

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app" style="margin-top: 0">
-    <Header></Header>
-    <router-view/>
+    <div class="container">
+      <Header></Header>
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -21,7 +23,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }

8af12de99ab7f12953b469e7da702e7d7e70b97d
Chien Hoang <hoangducchien97@gmail.com>
2019-09-25 14:12:57 +0700

    beforeRouteLeave

--- vue-router/src/components/user/UserEdit.vue
+++ vue-router/src/components/user/UserEdit.vue
@@ -10,15 +10,41 @@
         Browser: {{ $route.query.browser }}
       </li>
     </ul>
+    <button type="button" class="btn btn-danger"
+      @click="show"
+    >
+    Delete</button>
     <div style="height: 1700px"></div>
     <h3 id="react">Learn React with Sky Albert</h3>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data(){
+      return{
+        status: false
+      }
+    },
+    methods:{
+      show(){
+        this.status = true;
+        alert('OKEY');
+      }
+    },
+    // su kien nay xay khi truoc khi roi` bo router nay sang router khac
+    beforeRouteLeave (to, from, next) {
+      if(this.status){
+        next();
+      }else{
+        if(confirm('Are you sure ? Any data will not save !!!')){
+          next();
+        }else{
+          next(false);
+        }
+      }
+    }
+  }
 </script>
 
 <style>

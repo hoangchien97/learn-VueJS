74001c185d54d8ed45f3900c91f0f68b869086e4
Chien Hoang <hoangducchien97@gmail.com>
2019-09-25 15:36:50 +0700

    getting start vuex

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vuex/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vuex from './components/Vuex.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Vuex
   }
 }
 </script>
--- vuex/src/main.js
+++ vuex/src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import 'es6-promise/auto'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 1
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+// truy cap : store.state
+// kich hoat thay doi : store.commit
+
+store.commit('increment');
+console.log(store.state.count);
+
 
 new Vue({
   render: h => h(App),

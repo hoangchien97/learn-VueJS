36475b2300b087c43bf003b91f15fc2f08d3b9e6
Chien Hoang <hoangducchien97@gmail.com>
2019-09-25 11:20:22 +0700

    using transition when routes

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app" style="margin-top: 0">
-    <!-- Nhung page khac. header se khong hoat dong -->
     <router-view name="header"></router-view>
     <div class="container">
-    <router-view> </router-view>
+      <transition name="slide">
+        <router-view> </router-view>
+      </transition>
     </div>
   </div>
 </template>
@@ -19,12 +20,29 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /* text-align: center; */
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .slide-leave-active{
+    transition: opacity 0.3s ease;
+    opacity: 0;
+    animation: slide-out 0.3s ease-out forwards;
+  }
+  .slide-leave{
+    opacity: 1;
+    transform: translateX(0);
+  }
+  @keyframes slide-out{
+    0%{
+      transform: translateX(0)
+    }
+    100%{
+      transform: translateX(30px)
+    }
+  }
 </style>

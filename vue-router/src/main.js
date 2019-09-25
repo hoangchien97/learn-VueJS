2aad721bd86090716139153d634bae774a3bf346
Chien Hoang <hoangducchien97@gmail.com>
2019-09-25 11:51:47 +0700

    using scrollBehavior with hash

--- vue-router/src/main.js
+++ vue-router/src/main.js
@@ -7,7 +7,16 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: 'history',
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    if(to.hash){
+      return {selector: to.hash}
+    }
+    return { x: 0, y: 0 }
+  },
 })
 
 Vue.config.productionTip = false
